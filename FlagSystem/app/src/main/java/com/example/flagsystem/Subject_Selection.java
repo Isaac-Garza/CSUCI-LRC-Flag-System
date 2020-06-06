1d7153fcbe4dcc494361851b14eae277b045a322
ICHeadache <ICHeadache@hotmail.com>
2020-06-06 14:10:51 -0700

    Main Activity Created
    Created the Main Acitivity Page

---
+++ FlagSystem/app/src/main/java/com/example/flagsystem/Subject_Selection.java
@@ -0,0 +1,14 @@
+package com.example.flagsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Subject_Selection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject__selection);
+    }
+}
