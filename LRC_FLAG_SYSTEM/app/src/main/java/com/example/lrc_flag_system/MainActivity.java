10ce97b35e0520a7513cadf8b1d52b51a12d5b18
Isaac-Garza <Igarza957@yahoo.com>
2020-11-07 13:27:22 -0800

    Table Spinner Added

--- LRC_FLAG_SYSTEM/app/src/main/java/com/example/lrc_flag_system/MainActivity.java
+++ LRC_FLAG_SYSTEM/app/src/main/java/com/example/lrc_flag_system/MainActivity.java
@@ -8,14 +8,20 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    CardView pageTutorButton;
+    Spinner tableSpinner;
+    String tableString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CardView pageTutorButton = findViewById(R.id.pageTutor);
+        pageTutorButton = findViewById(R.id.pageTutor);
+        tableSpinner = findViewById(R.id.tableSpinner);
 
         pageTutorButton.setOnClickListener(this);
 
@@ -24,7 +30,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v)
     {
-        Intent i = new Intent(this,Subject.class);
-        startActivity(i);
+        Intent intent = new Intent(this,Subject.class);
+        tableString = tableSpinner.getSelectedItem().toString();
+        intent.putExtra("TableNumber", tableString);
+        startActivity(intent);
     }
 }
