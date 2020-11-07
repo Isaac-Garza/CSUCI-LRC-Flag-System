3caee1ee34f580fcca2a78ddbfa39ee56bb138a1
Isaac-Garza <Igarza957@yahoo.com>
2020-11-07 13:28:21 -0800

    Update Table from Firebase

--- LRC_FLAG_SYSTEM/app/src/main/java/com/example/lrc_flag_system/WaitingRoom.java
+++ LRC_FLAG_SYSTEM/app/src/main/java/com/example/lrc_flag_system/WaitingRoom.java
@@ -8,7 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class WaitingRoom extends AppCompatActivity implements View.OnClickListener {
+
+    TableModel tableFlagged;
+
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,8 @@ public class WaitingRoom extends AppCompatActivity implements View.OnClickListen
         setContentView(R.layout.activity_waiting_room);
         Intent intent = getIntent();
         String button = intent.getStringExtra("pressed_button");
+        tableFlagged = intent.getParcelableExtra("TableValue");
+
         String selectedSubject = "A %s Tutor has been Requested";
         TextView titleName = findViewById(R.id.request);
         selectedSubject = String.format(selectedSubject, button);
@@ -27,6 +38,11 @@ public class WaitingRoom extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
+
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference("Table");
+        reference.child(tableFlagged.getTableNumber()).removeValue();
+
         Intent i = new Intent(this,Subject.class);
         startActivity(i);
     }
